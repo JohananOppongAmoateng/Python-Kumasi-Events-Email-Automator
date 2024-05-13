e16780cd1d32835f09d735194956156141ff84e6
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2024-05-13 01:12:58 +0000

    email support

--- events_email_automator/settings.py
+++ events_email_automator/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -151,4 +151,10 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.get_env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.get_env("EMAIL_HOST_PASSWORD")
 # AUTH_USER_MODEL = "registration.CustomUser"
